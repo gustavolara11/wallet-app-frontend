5faf9762b2eca7e0409b0e86fe653be85c487323
gustavolara11 <gustavex2007@gmail.com>
2023-03-20 18:06:33 -0300

    feat: implement header at home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,29 @@
-window.onload = () => {
+const onLoadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navbarUserInfo = document.getElementById("navbar-user-container");
+  const navbarUserAvatar = document.getElementById("navbar-user-avatar");
+
+  // add user email
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+  navbarUserInfo.appendChild(emailElement);
+
+  // add logout link
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("Sair");
+  logoutElement.appendChild(logoutText);
+  navbarUserInfo.appendChild(logoutElement);
+
+  // add user first letter inside avatar
+  const nameElement = document.createElement("h2");
+  const nameText = document.createTextNode(name.charAt(0));
+  nameElement.appendChild(nameText);
+  navbarUserAvatar.appendChild(nameElement);
+};
+
+window.onload = () => {
+  onLoadUserInfo();
 };
