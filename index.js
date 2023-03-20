285a03ec53ac14ae2b2aa1d3338c04b011875790
gustavolara11 <gustavex2007@gmail.com>
2023-03-20 15:15:34 -0300

    feat: integrate user initial validation

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const onClickLogin = () => {
+const validadeUser = async (email) => {
+  try {
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+    );
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const onClickLogin = async () => {
   const email = document.getElementById("imput-email").value;
   if (email.length < 5 || !email.includes("@")) {
     alert("E-mail inválido!");
     return;
   }
-  window.open("/pages/home/index.html");
+
+  const result = await validadeUser(email);
+  if (result.error) {
+    alert("Falha ao validar e-mail.");
+    return;
+  }
+
+  localStorage.setItem("@WalletApp:userEmail", result.email);
+  localStorage.setItem("@WalletApp:userName", result.name);
+  localStorage.setItem("@WalletApp:userId", result.id);
+  window.open("src/pages/home/index.html", "_self");
 };
