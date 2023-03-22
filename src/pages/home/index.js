ebfef627ebcac8401a6d73af76826fdce287b4c8
gustavolara11 <gustavex2007@gmail.com>
2023-03-22 12:04:15 -0300

    feat: implement table data

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,60 @@
+const renderFinancesList = (data) => {
+  const table = document.getElementById("finances-table");
+  /**
+  <tr>
+    <td>texto 1</td>
+    <td>texto 2</td>
+    <td>texto 3</td>
+    <td class="center">texto 4</td>
+    <td class="right">texto 5</td>
+  </tr>
+   */
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    // title
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    // category
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    // date
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(new Date(item.date).toISOString());
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    // value
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    // delete
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("Deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    // table add tablerow
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinancesElements = (data) => {
   const totalItems = data.length;
   const revenues = data
@@ -59,7 +116,7 @@ const renderFinancesElements = (data) => {
 
 const onLoadFinancesData = async () => {
   try {
-    const date = "2022-12-15";
+    const date = "2022-12-25";
     const email = localStorage.getItem("@WalletApp:userEmail");
     const result = await fetch(
       `https://mp-wallet-app-api.herokuapp.com/finances?date=${date}`,
@@ -72,6 +129,7 @@ const onLoadFinancesData = async () => {
     );
     const data = await result.json();
     renderFinancesElements(data);
+    renderFinancesList(data);
     return data;
   } catch (error) {
     return { error };
