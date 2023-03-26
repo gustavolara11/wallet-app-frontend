9beb5439024df589b40c360754e791c02102c454
gustavolara11 <gustavex2007@gmail.com>
2023-03-26 19:24:56 -0300

    feat: implement dynamic date and logout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,23 @@
+const onLogout = () => {
+  localStorage.clear();
+  window.open("../../../index.html", "_self");
+};
+
+const onDeleteItem = async (id) => {
+  try {
+    const email = localStorage.getItem("@WalletApp:userEmail");
+    await fetch(`https://mp-wallet-app-api.herokuapp.com/finances/${id}`, {
+      method: "DELETE",
+      headers: {
+        email: email,
+      },
+    });
+    onLoadFinancesData();
+  } catch (error) {
+    alert("Erro ao deletar item.");
+  }
+};
+
 const renderFinancesList = (data) => {
   const table = document.getElementById("finances-table");
   table.innerHTML = "";
@@ -51,7 +71,9 @@ const renderFinancesList = (data) => {
 
     // date
     const dateTd = document.createElement("td");
-    const dateText = document.createTextNode(new Date(item.date).toISOString());
+    const dateText = document.createTextNode(
+      new Date(item.date).toISOString().split("T")[0]
+    );
     dateTd.appendChild(dateText);
     tableRow.appendChild(dateTd);
 
@@ -69,6 +91,8 @@ const renderFinancesList = (data) => {
 
     // delete
     const deleteTd = document.createElement("td");
+    deleteTd.style.cursor = "pointer";
+    deleteTd.onclick = () => onDeleteItem(item.id);
     deleteTd.className = "right";
     const deleteText = document.createTextNode("Deletar");
     deleteTd.appendChild(deleteText);
@@ -168,10 +192,10 @@ const renderFinancesElements = (data) => {
 
 const onLoadFinancesData = async () => {
   try {
-    const date = "2022-12-25";
+    const dateInputValue = document.getElementById("select-date").value;
     const email = localStorage.getItem("@WalletApp:userEmail");
     const result = await fetch(
-      `https://mp-wallet-app-api.herokuapp.com/finances?date=${date}`,
+      `https://mp-wallet-app-api.herokuapp.com/finances?date=${dateInputValue}`,
       {
         method: "GET",
         headers: {
@@ -203,6 +227,8 @@ const onLoadUserInfo = () => {
 
   // add logout link
   const logoutElement = document.createElement("a");
+  logoutElement.onclick = () => onLogout();
+  logoutElement.style.cursor = "pointer";
   const logoutText = document.createTextNode("Sair");
   logoutElement.appendChild(logoutText);
   navbarUserInfo.appendChild(logoutElement);
@@ -288,10 +314,21 @@ const onCreateFinanceRelease = async (target) => {
   }
 };
 
+const setInitialDate = () => {
+  const dateInput = document.getElementById("select-date");
+  const nowDate = new Date().toISOString().split("T")[0];
+  dateInput.value = nowDate;
+  dateInput.addEventListener("change", () => {
+    onLoadFinancesData();
+  });
+};
+
 window.onload = () => {
+  setInitialDate();
   onLoadUserInfo();
   onLoadFinancesData();
   onLoadCategories();
+
   const form = document.getElementById("form-finance-release");
   form.onsubmit = (event) => {
     event.preventDefault();
